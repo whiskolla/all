6389cecc23acca6c844e3992a1423235f95adc3b
Anastasia <87172939+whiskolla@users.noreply.github.com>
2023-06-08 12:12:09 +0300

    Add files via upload

---
+++ Buysell-Lesson3/Buysell-Lesson3/src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,23 @@
+package com.example.buysell.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+}
